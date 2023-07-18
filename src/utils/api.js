9e2742699ed618b0ffb1e1195e93efb11f5b30d0
gauravsir34 <119123994+gauravsir34@users.noreply.github.com>
2023-07-18 23:08:10 +0530

    Add files via upload

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "b9a3ca3442msh14ebc9482bbb5a6p16aedajsnf65d859a751f",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
